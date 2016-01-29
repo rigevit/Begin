09e30efd7ac854f717931ade0504daa878e00ab9
rigevit <velles@meta.ua>
2016-01-29 16:12:29 +0200

    commit by veles 20160129 Робота з умовними операторами

---
+++ src/WorkWithConditionalOperators.java
@@ -0,0 +1,15 @@
+/**
+ * Created by veles on 29.01.2016.
+ */
+public class WorkWithConditionalOperators {
+    public static void main (String [] args) {
+        int a, b;
+        double c;
+        a = 100;
+        b = 100;
+        c = a * b;
+        if (a > b) System.out.print("a більше b");
+        if (a < b) System.out.print("a менше b");
+        else System.out.print("a дорівнює b");
+    }
+}
