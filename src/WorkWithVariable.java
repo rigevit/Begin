0444685547e6d6a8895053f2910d7c096f0253c3
rigevit <velles@meta.ua>
2016-01-29 15:40:08 +0200

    commit by veles 20160129

--- src/WorkWithVariable.java
+++ src/WorkWithVariable.java
@@ -3,10 +3,15 @@
  */
 public class WorkWithVariable {
     public static void main (String[] args) {
-        int a, b, c;
+        int a, b;
+        double c;
         a = 100;
         b = 123;
         c = a * b;
-        System.out.print("Підрахунок значення с: " + c);
+        System.out.println("Підрахунок значення с: " + c);
+        c = c * 5;
+        System.out.println("Множення значення с на 5: " + c);
+        c = c / 513.6;
+        System.out.print("Поділити значення с на 513.6: " + c);
     }
 }
